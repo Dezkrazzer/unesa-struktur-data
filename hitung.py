f4234252e7b82e1edfd06a1c68e76f2d62d2fb60
Dezkrazzer <uqie.vanmadiun@gmail.com>
2025-02-24 20:19:26 +0700

    add(hitung): Initial commit for hitung py

---
+++ hitung.py
@@ -0,0 +1,8 @@
+def jumlah(a, b):
+    return a + b
+
+angka1 = int(input("Masukkan angka pertama: "))
+angka2 = int(input("Masukkan angka kedua: "))
+
+hasil = jumlah(angka1, angka2)
+print(f"Hasil penjumlahan: {hasil}")
