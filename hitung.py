314c11af2a5dbf327e1247ebe6227a1058e15084
Dezkrazzer <uqie.vanmadiun@gmail.com>
2025-02-24 20:59:43 +0700

    chore(hitung, kalkulator): Added style arrow

--- hitung.py
+++ hitung.py
@@ -1,8 +1,8 @@
 def jumlah(a, b):
     return a + b
 
-angka1 = int(input("Masukkan angka pertama: "))
-angka2 = int(input("Masukkan angka kedua: "))
+angka1 = int(input("» Masukkan angka pertama: "))
+angka2 = int(input("» Masukkan angka kedua: "))
 
 hasil = jumlah(angka1, angka2)
 print(f"Hasil penjumlahan: {hasil}")
--- kalkulator.py
+++ kalkulator.py
@@ -2,9 +2,9 @@ print("===================================")
 print("         KALKULATOR PHYTON         ")
 print("===================================")
 
-angka_1 = int(input("Masukkan angka pertama: "))
-angka_2 = int(input("Masukkan angka kedua: "))
-operator = input("\n[1] Tambah\n[2] Kurang\n[3] Kali\n[4] Bagi\n\nPilih Operator (1/2/3/4): ")
+angka_1 = int(input("» Masukkan angka pertama: "))
+angka_2 = int(input("» Masukkan angka kedua: "))
+operator = input("\n[1] Tambah\n[2] Kurang\n[3] Kali\n[4] Bagi\n\n» Pilih Operator (1/2/3/4): ")
 
 if operator == "1":
     tambah = angka_1 + angka_2
@@ -19,4 +19,4 @@ elif operator == "4":
     bagi = angka_1 / angka_2
     print(f"Hasil {angka_1} / {angka_2} = {bagi}")
 else:
-    print("Operator yang Anda pilih tidak valid")
+    print("[ERROR] Operator yang Anda pilih tidak valid")
